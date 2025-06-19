cfcf7cd1b0cc97ad7ad9281864c63e897409d461
Supundi Dinaya <supundidinaya@gmail.com>
2025-06-19 11:29:35 +0530

    Add files via upload

---
+++ Edge.java
@@ -0,0 +1,38 @@
+
+public class Edge {
+    private final int from;
+    private final int to;
+    private final int capacity;
+    private int flow;
+
+    public Edge(int from, int to, int capacity) {
+        this.from = from;
+        this.to = to;
+        this.capacity = capacity;
+        this.flow = 0;
+    }
+
+    public int getFrom() {
+        return from;
+    }
+
+    public int getTo() {
+        return to;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public int getFlow() {
+        return flow;
+    }
+
+    public void setFlow(int flow) {
+        this.flow = flow;
+    }
+
+    public int getResidualCapacity() {
+        return capacity - flow;
+    }
+}
